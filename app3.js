760cb1590bf1cf7ad9a3d84c36e3d12ae8ca1f9f
eziskin <eliziskin@gmail.com>
2019-04-26 00:15:28 -0700

    finalized resume

---
+++ app3.js
@@ -0,0 +1,18 @@
+console.log('Eli Ziskin');
+console.log('Scientist');
+console.log('I enjoy long walks on sidewalks');
+
+console.log('Philosophy');
+console.log('Walking');
+console.log('People-watching');
+console.log('Daydreaming');
+
+console.log('Cognitive science research at two universities and four labs, examining visual, spatial, language, and time cognition');
+console.log('Tutor for five years, in Biology');
+console.log('Founder and president of a few organizations - Scholars of Philosophy, Cognitive Science Student Association, and Inter-Club Council');
+
+console.log('Russian');
+console.log('Violin playing');
+console.log('Experiment design');
+console.log('Science writing');
+console.log('Project Management');
